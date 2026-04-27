0a6949cfcd679ef2020902b0daff5653ed003dc6
Samuel Cardona <samuelcardonaog@gmail.com>
2026-04-26 20:26:07 -0500

    Creacion de usuarioResponseDTO

---
+++ src/main/java/com/materiales/materiales/dto/usuarioResponseDTO.java
@@ -0,0 +1,11 @@
+package com.materiales.materiales.dto;
+
+import lombok.Data;
+
+@Data
+public class usuarioResponseDTO {
+    private Long idUsuario;
+    private String nombre;
+    private String correo;
+
+}
